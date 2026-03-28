7c266fcdeb8bb83926164a17aeb80da547b147e4
Thatboiobah <osmpopoola@gmail.com>
2026-03-28 04:54:14 -0700

    add user.js and update it

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import testRoutes from './routes/testRoutes.js' // <-- add this
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: 'GreenCore API is running' })
 })
+
+app.use('/api', testRoutes) // <-- mount /api routes
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
